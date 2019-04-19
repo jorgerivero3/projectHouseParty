632cfb63842046541c2c3c2e36958d6e0161904f
Jorge Rivero <riverojorge145@gmail.com>
2019-04-19 13:50:14 -0500

    Initial Commit

---
+++ projectHouseParty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projectHouseParty
+//
+//  Created by Jorge Rivero III on 4/19/19.
+//  Copyright © 2019 cs329e. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
